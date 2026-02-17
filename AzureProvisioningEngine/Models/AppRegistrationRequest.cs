cbf8bbbfb2c7c5ae34b6ac3fef0066cfa77fe9bd
Priyabrata Pattanayak <priyabrata.cetb@gmail.com>
2026-02-17 15:32:45 +1100

    working

--- AzureProvisioningEngine/Models/AppRegistrationRequest.cs
+++ AzureProvisioningEngine/Models/AppRegistrationRequest.cs
@@ -1,17 +1,27 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace AzureProvisioningEngine.Models
 {
     public class AppRegistrationRequest
     {
+        [Required]
         public string AppName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string OwnerEmail { get; set; }
-        public string Description { get; set; }
-        public string SignInAudience { get; set; } // AzureADMyOrg, AzureADMultipleOrgs
-        public string[] RedirectUris { get; set; }
-        public bool GenerateClientSecret { get; set; }
+
+        [Required]
         public string BusinessJustification { get; set; }
+
+        public string SignInAudience { get; set; } = "AzureADMyOrg"; // Default to Single Tenant
+
+        public List<string> RedirectUris { get; set; }
+
+        public string Description { get; set; }
+
+        public bool GenerateClientSecret { get; set; } = true;
     }
 
     public class AppRegistrationResult
@@ -19,8 +29,8 @@ namespace AzureProvisioningEngine.Models
         public string AppId { get; set; }
         public string ObjectId { get; set; }
         public string DisplayName { get; set; }
-        public string ClientSecret { get; set; } // Only returned if generated
-        public DateTimeOffset? SecretExpiration { get; set; }
+        public string ClientSecret { get; set; }
+        public System.DateTimeOffset? SecretExpiration { get; set; }
         public string Status { get; set; }
     }
 }
--- AzureProvisioningEngine/Program.cs
+++ AzureProvisioningEngine/Program.cs
@@ -1,37 +1,65 @@
 using AzureProvisioningEngine.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-// Explicitly use the full namespace to avoid ambiguity with Microsoft.Graph.Models.WebApplication
-var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Register the Provisioning Service
-builder.Services.AddScoped<IAzureProvisioningService, AzureProvisioningService>();
-
-// Register the Notification Service
-builder.Services.AddScoped<INotificationService, EmailNotificationService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace AzureProvisioningEngine
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpClient(); // Register IHttpClientFactory
+
+            // Register application services
+            services.AddScoped<IAzureProvisioningService, AzureProvisioningService>();
+            services.AddScoped<INotificationService, EmailNotificationService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
